175f7f68fb9d4df41d8a9dd441d5865cc915c6f7
Zelenin Dmitry <3elenin.ds@gmail.com>
2024-05-16 23:05:03 +0500

    code according to rukes

--- main.go
+++ main.go
@@ -45,51 +45,52 @@ func special(charName, charClass string) string {
 	}
 	if charClass == "healer" {
 		return fmt.Sprintf("%s применил специальное умение `Защита %d`", charName, 10+30)
-	{
+	}
 		return "неизвестный класс персонажа"
 }
 
 // здесь обратите внимание на имена параметров
+
 func startTraining(charName, charClass string) string {
-	if charClass == "warrior" {
-		fmt.Printf("%s, ты Воитель - отличный боец ближнего боя.\n", charName)
-	}
+    if charClass == "warrior" {
+        fmt.Printf("%s, ты Воитель - отличный боец ближнего боя.\n", charName)
+    }
 
-	if charClass == "mage" {
-		fmt.Printf("%s, ты Маг - превосходный укротитель стихий.\n", charName)
-	}
+    if charClass == "mage" {
+        fmt.Printf("%s, ты Маг - превосходный укротитель стихий.\n", charName)
+    }
 
-	if charClass == "healer" {
-		fmt.Printf("%s, ты Лекарь - чародей, способный исцелять раны.\n", charName)
-	}
+    if charClass == "healer" {
+        fmt.Printf("%s, ты Лекарь - чародей, способный исцелять раны.\n", charName)
+    }
 
-	fmt.Println("Потренируйся управлять своими навыками.")
-	fmt.Println("Введи одну из команд: attack — чтобы атаковать противника,")
-	fmt.Println("defence — чтобы блокировать атаку противника,")
-	fmt.Println("special — чтобы использовать свою суперсилу.")
-	fmt.Println("Если не хочешь тренироваться, введи команду skip.")
+    fmt.Println("Потренируйся управлять своими навыками.")
+    fmt.Println("Введи одну из команд: attack — чтобы атаковать противника,")
+    fmt.Println("defence — чтобы блокировать атаку противника")
+    fmt.Println("special — чтобы использовать свою суперсилу.")
+    fmt.Println("Если не хочешь тренироваться, введи команду skip.")
 
-	var cmd string
-	for cmd != "skip" {
-		fmt.Print("Введи команду: ")
-		fmt.Scanf("%s\n", &cmd)
-
-		switch {
-		case cmd == "attack":
-			fmt.Println(attack(charName, charClass))
-		case cmd == "defence":
-			fmt.Println(defence(charName, charClass))
-		case cmd == "special":
-			fmt.Println(special(charName, charClass))
-		default:
-			fmt.Println("неизвестая команда")
-		}
-	}
-	return "тренировка окончена"
+    var cmd string
+    for cmd != "skip" {
+        fmt.Print("Введи команду: ")
+        fmt.Scanf("%s\n", &cmd)
+        switch {
+        case cmd == "attack":
+            fmt.Println(attack(charName, charClass))
+        case cmd == "defence":
+            fmt.Println(defence(charName, charClass))
+        case cmd == "special":
+            fmt.Println(special(charName, charClass))
+        default:
+            fmt.Println("неизвестная команда")
+        }
+    }
+    return "тренировка окончена"
 }
 
+
 // обратите внимание на имя функции и имена переменных
-func choiseCharClass() string {
+func choiceCharClass() string {
 	var approveChoice string
 	var charClass string
 
@@ -106,7 +107,7 @@ func choiseCharClass() string {
 			fmt.Println("Лекарь — могущественный заклинатель. Черпает силы из природы, веры и духов.")
 		}
 		fmt.Print("Нажми (Y), чтобы подтвердить выбор, или любую другую кнопку, чтобы выбрать другого персонажа: ")
-		fmt.Scanf("%s\n", &approve_choice)
+		fmt.Scanf("%s\n", &approveChoice)
 		approveChoice = strings.ToLower(approveChoice)
 	}
 	return charClass
@@ -126,7 +127,7 @@ func main() {
 	fmt.Println("Ты можешь выбрать один из трёх путей силы:")
 	fmt.Println("Воитель, Маг, Лекарь")
 
-	charClass := choiseCharClass()
+	charClass := choiceCharClass()
 
 	fmt.Println(startTraining(charName, charClass))
 }
